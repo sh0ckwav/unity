a05efc35078a3161336a6e44ef0f397cfb361a1c
Stephen Stadler <stephen.stadler@nike.com>
2016-10-22 19:35:05 -0700

    Adding scripts and ghost

---
+++ Jumpman/Assets/Scripts/FoodScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FoodScript : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D co) {
+        if (co.name == "pacman")
+            Destroy(gameObject);
+    }
+}
